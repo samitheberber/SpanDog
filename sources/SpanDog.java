26d5042958daf7b872995e6112899bd2ecb2466e
Sami Saada <sami.saada@gmail.com>
2010-02-26 01:41:00 +0200

    Initial commit, which creates the environment.

---
+++ sources/SpanDog.java
@@ -0,0 +1,7 @@
+import lejos.nxt.Button;
+
+public class SpanDog{
+    public static void main (String[] args) {
+        System.out.println("Moooi"); Button.ENTER.waitForPressAndRelease(); 
+    } 
+}
